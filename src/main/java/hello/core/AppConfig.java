5a80c0a54b1e57972999313e841eea88efb6a342
awbwd4 <awbwd4@gmail.com>
2022-05-25 19:45:13 +0900

    22.05.25 AppConfig

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixedDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,15 +14,25 @@ public class AppConfig {
     // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
     //생성한 객체의 인스턴스 참조를 ""생성자""를 통해서 주입해준다.
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixedDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+
+
     // 생성자 주입
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixedDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
 
 
 }
