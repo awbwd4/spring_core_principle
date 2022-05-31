d68d26d95c53bf56eb36bf495d78cb21ac3a41cf
awbwd4 <awbwd4@gmail.com>
2022-06-01 01:02:14 +0900

    22.06.01 component scan

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,17 +1,27 @@
 package hello.core;
 
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = {"hello.core.member","hello.service"},//이 패키지 하위에서만 탐색한다.
+//        basePackageClasses = AutoAppConfig.class, // 이 클래스가 위치한 패키지에서 탐색한다.
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes=Configuration.class)
         // Configuration 애노테이션이 붙은 클래스는 스캐닝에서 배제한다.
+        // 디폴트 탐색위치는? ComponentScan 어노테이션이 붙은 클래스가 위치한 패키지에서 부터 탐색 시작.
+        // hello.core 를 전부 뒤진다.
 )
 public class AutoAppConfig {
 
-
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication// hello.core 패키지에서부터 그 하위 패키지로 스캐닝을 시작함
 public class CoreApplication {
 
 	public static void main(String[] args) {
