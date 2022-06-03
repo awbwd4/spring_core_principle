6f6b793dbe52ac588bfe821c6704f0f3ac5553d7
awbwd4 <awbwd4@gmail.com>
2022-06-03 21:06:39 +0900

    22.06.03 recap

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -14,24 +14,35 @@ public class OrderApp {
 
     public static void main(String[] args) {
 
-//        AppConfig appConfig = new AppConfig();
+        AppConfig appConfig = new AppConfig();
 //
 //        MemberService memberService = appConfig.memberService();
 //        OrderService orderService = appConfig.orderService();
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        //AppConfig에 있는 환경 설정 정보를 가지고
-        // 스프링이 스프링 빈으로 등록된 것들을 객체 생성해줌.
+//        //AppConfig에 있는 환경 설정 정보를 가지고
+//        // 스프링이 스프링 빈으로 등록된 것들을 객체 생성해줌.
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+//
+
+
+
+
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
+
+//        Member member1 = memberService.findMember(memberId);
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
         System.out.println("Order = " + order);
 
     }
+
+
+
+
 }
